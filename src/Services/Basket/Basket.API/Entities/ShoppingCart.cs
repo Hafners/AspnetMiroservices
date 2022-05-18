2dbf19db52c6e9ea3cdfd4177ce47b96d79eab26
Stefan Hafner <Stefan.Hafner@DynaDevelopment.onmicrosoft.com>
2022-05-18 11:23:22 +0200

    added basket service

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Items.Sum(x => x.Price * x.Quantity);
+            }
+        }
+    }
+}
